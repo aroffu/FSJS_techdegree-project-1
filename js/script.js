869c34befc2f4c9ef7f291e9f4fd81fccf23ee5a
aroffu <32528090+aroffu@users.noreply.github.com>
2020-09-11 01:56:06 +0200

    implemented every function
    added everything else

--- js/script.js
+++ js/script.js
@@ -14,11 +14,13 @@ project 1 - A Random Quote Generator
 const quotes = [
 	{
 		quote: "Have no fear of perfection - you'll never reach it.",
-		source: 'Salvador Dalì'
+		source: 'Salvador Dalì',
+		tag: 'art'
 	},
 	{
 		quote: 'Computers are useless.They can only give you answers.',
-		source: 'Pablo Picasso'
+		source: 'Pablo Picasso',
+		tag: 'art'
 	},
 	{
 		quote:
@@ -29,21 +31,24 @@ const quotes = [
 	},
 	{
 		quote:
-			"We made a solemn vow not to take any job outside of show business. We borrowed money from parents and friends, rather than take that lethal job waiting tables. This forced us to take any job offered to us. Anything. We once did a show in the middle of the Benjamin Franklin Parkway in Philadelphia as part of a fashion show on a hot July night while all around our stage, a race-riot was fully underway. That's how serious we were about our vow.",
-		source: 'Raymond Joseph Teller',
-		citation: 'Fury Letter written to Brian Brushwood',
-		year: 1993
+			"Everyone should think he's good enough to write a quote",
+		source: 'Matteo Aroffu',
+		citation: 'Techdegree FSJS - Project 1 ',
+		year: 2020,
+		tag: 'fancy'
 	},
 	{
 		quote: 'A man cannot be too careful in the choice of his enemies.',
 		source: 'Oscar Wilde',
 		citation: 'The Picture of Dorian Gray',
-		year: 1891
+		year: 1891,
+		tag: 'literature'
 	},
 	{
 		quote: 'Violence is the last refuge of the incompetent.',
 		source: 'Isaac Asimov',
-		citation: 'Salvor Hardin in "Foundation"'
+		citation: 'Salvor Hardin in "Foundation"',
+		tag: 'literature'
 	}
 ];
 
@@ -52,16 +57,12 @@ const quotes = [
 ***/
 
 //returns a integer between 0 (inclusive) and max (exclusive) suitable for array indexes
-function getRandomIndex(max) {
-	return Math.floor(Math.random() * Math.floor(max));
-}
+let getRandomIndex = (max) => Math.floor(Math.random() * Math.floor(max));
 
-/***
- * given an array and a function like 'getRandomIndex' 
- * returns a random object from the array
-***/
+//returns a given array element
 function getRandomQuote(array) {
-	let randomIndex = getRandomIndex(array.lenght);
+	let randomIndex = getRandomIndex(array.length);
+	//hint: index 3 gives you my favourite and possibly the best quote, you're welcome
 	return array[randomIndex];
 }
 
@@ -69,11 +70,51 @@ function getRandomQuote(array) {
  * `printQuote` function
 ***/
 
-//object.hasOwnProperty(key)
+function printQuote() {
+	const quote_box = document.querySelector('.quote-box');
+	//holds HTML code to be printed
+	let printedQuote = '';
+	let quoteObject = getRandomQuote(quotes);
+
+	/** The following code builds the message to be printed
+	 * quote and source are always present
+	 * 'if' statements check the presence of other properties and eventually add the relative code to 'printedQuote'
+	 * closing p tag is found after 
+	**/
+	printedQuote = `
+    <p class="quote">${quoteObject.quote}</p>
+	<p class="source">${quoteObject.source}`;//missing </p> is found after if statements
+
+	if (quoteObject.hasOwnProperty('citation')) {
+		printedQuote += `<span class="citation">${quoteObject.citation}</span>`;
+	}
+	if (quoteObject.hasOwnProperty('year')) {
+		printedQuote += `<span class="year">${quoteObject.year}</span>`;
+	}
+	if (quoteObject.hasOwnProperty('tag')) {
+		printedQuote += `<span class="tag">${quoteObject.tag}</span>`;
+	}
+
+	//closing tag missing from the line above if statements
+	printedQuote += `</p>`;
+
+	//prints actual message
+	quote_box.innerHTML = printedQuote;
+
+	//changes background-color on every call 
+	document.querySelector('body').style.backgroundColor = `rgb(${getRandomIndex(255)}, ${getRandomIndex(255)}, ${getRandomIndex(255)})`;
+}
+
+//prints a first quote right after the page loads otherwise it will be empty
+printQuote();
+
+//prints a quote every 5 seconds
+setInterval(printQuote, 5000);
 
 /***
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
 
-//document.getElementById('load-quote').addEventListener('click', printQuote, false);
+document.getElementById('load-quote').addEventListener('click', printQuote, false);
+
